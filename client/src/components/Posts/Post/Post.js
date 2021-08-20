f2396b751e31fbdeede88a7c840132a32dbaa39f
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-08-20 10:38:33 +0200

    post and posts

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,9 +1,37 @@
 import React from "react";
 import useStyles from "./styles";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
-const Post = () => {
+import ThumbUpAltIcon from "@material-ui/icons";
+import DeleteIcon from "@material-ui/icons";
+import MoreHorizicon from "@material-ui/icons";
+
+import moment from "moment";
+
+const Post = ({ post }) => {
   const classes = useStyles();
-  return <h1>Post</h1>;
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={post.selectedFile}
+        title={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">
+          {moment(post.createdAt).fromNow()}
+        </Typography>
+      </div>
+    </Card>
+  );
 };
 
 export default Post;
