8723f7936f07c436e39637805b9ec180bce8cd12
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-08-20 11:33:24 +0200

    design of the post is done

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -9,9 +9,9 @@ import {
   Typography,
 } from "@material-ui/core";
 
-import ThumbUpAltIcon from "@material-ui/icons";
-import DeleteIcon from "@material-ui/icons";
-import MoreHorizicon from "@material-ui/icons";
+import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MoreHorizicon from "@material-ui/icons/MoreHoriz";
 
 import moment from "moment";
 
@@ -30,6 +30,46 @@ const Post = ({ post }) => {
           {moment(post.createdAt).fromNow()}
         </Typography>
       </div>
+
+      <div className={classes.overlay2}>
+        <Button style={{ color: "white" }} size="small" onClick={() => {}}>
+          <MoreHorizicon fontSize="default" />
+        </Button>
+      </div>
+
+      <div className={classes.details}>
+        <Typography variant="body2" color="textSecondary" component="h2">
+          {post.tags.map((tag) => `#${tag}`)}
+        </Typography>
+      </div>
+
+      <Typography
+        className={classes.title}
+        gutterBottom
+        variant="h5"
+        component="h2"
+      >
+        {post.title}
+      </Typography>
+
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {post.message}
+        </Typography>
+      </CardContent>
+
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" onClick={() => {}}>
+          <ThumbUpAltIcon fontSize="small" />
+          &nbsp; Like &nbsp;
+          {post.likeCount}
+        </Button>
+
+        <Button size="small" color="primary" onClick={() => {}}>
+          <DeleteIcon fontSize="small" />
+          Delete
+        </Button>
+      </CardActions>
     </Card>
   );
 };
