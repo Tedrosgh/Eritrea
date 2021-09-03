06ce13140d6fdba21ebc853c50f20ad7ecf4957f
Dilshod Rahmatov <dilshod.rahmatov@gmail.com>
2021-09-03 10:58:06 +0200

    deployment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/postsRoutes.js";
 
+import dotenv from "dotenv";
+
 const app = express();
+dotenv.config();
 
 // localhost:5000/posts
 
@@ -13,14 +16,15 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://dilshodDCI:dilshodDCI123@cluster0.ldws3.mongodb.net/imageboard46-1?retryWrites=true&w=majority";
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
+});
 
 //Add server port
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
